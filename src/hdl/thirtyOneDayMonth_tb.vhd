682fad4e8c6c4fa52294da005d7911cc01f67ab7
C26JulianKing <github noreply email@users.noreply.github.com>
2024-02-27 23:44:21 -0700

    First commit with Functionality

--- src/hdl/thirtyOneDayMonth_tb.vhd
+++ src/hdl/thirtyOneDayMonth_tb.vhd
@@ -50,37 +50,40 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
   
-entity thirtyOneDayMonth_tb is --notice entity is empty.  The testbench has no external connections.
-end thirtyOneDayMonth_tb;
+entity sevenSegDecoder_tb is --notice entity is empty.  The testbench has no external connections.
+end sevenSegDecoder_tb;
 
-architecture test_bench of thirtyOneDayMonth_tb is 
+architecture test_bench of sevenSegDecoder_tb is 
 	
   -- declare the component of your top-level design unit under test (UUT) (looks very similar to entity declaration)
-  component thirtyoneDayMonth is
+  component sevenSegDecoder is
     port(
-	i_A : in std_logic;
-	i_B : in std_logic;
-	i_C : in std_logic;
-	i_D : in std_logic;
-	o_Y : out std_logic
+	i_D : in std_logic_vector (3 downto 0);
+	o_S : out STD_LOGIC_VECTOR (6 downto 0)
     );	
   end component;
 
   -- declare any additional components required
   
   signal w_sw : std_logic_vector (3 downto 0):= (others=> '0');
-  signal w_Y : std_logic := '0';
+  signal w_seg : std_logic_vector (6 downto 0):= (others=> '0');
 
   
 begin
 	-- PORT MAPS ----------------------------------------
 	-- map ports for any component instances (port mapping is like wiring hardware)
-    thirtyOneDayMonthMux_inst : thirtyOneDayMonth port map (
-			i_D => w_sw(3),
-			i_C => w_sw(2),
-			i_B => w_sw(1),
-			i_A => w_sw(0),
-	    		o_Y => w_Y
+    sevenSegDecoder1_inst : sevenSegDecoder port map (
+			i_D(3) => w_sw(3),
+			i_D(2) => w_sw(2),
+			i_D(1) => w_sw(1),
+			i_D(0) => w_sw(0),
+			o_S(1) => w_seg(1),
+			o_S(2) => w_seg(2),
+			o_S(3) => w_seg(3),
+			o_S(4) => w_seg(4),
+			o_S(5) => w_seg(5),
+			o_S(6) => w_seg(6),
+			o_S(0) => w_seg(0)
         );
 	-----------------------------------------------------
 
@@ -91,37 +94,37 @@ begin
 	begin
 	-- Place test cases here. The first two have been written for you
 		w_sw <= x"0"; wait for 10 ns;
-            assert w_Y = '0' report "error on x0" severity failure;
+            assert w_seg = "1000000" report "error on x0" severity failure;
         w_sw <= x"1"; wait for 10 ns;
-            assert w_Y = '1' report "error on Jan" severity failure; 
+            assert w_Y = "1111001" report "error on Jan" severity failure; 
         w_sw <= x"2"; wait for 10 ns;
-            assert w_Y = '0' report "error on Feb" severity failure;
+            assert w_Y = "0100100" report "error on Feb" severity failure;
         w_sw <= x"3"; wait for 10 ns;
-            assert w_Y = '1' report "error on Mar" severity failure;
+            assert w_Y = "0110000" report "error on Mar" severity failure;
         w_sw <= x"4"; wait for 10 ns;
-            assert w_Y = '0' report "error on Apr" severity failure;
+            assert w_Y = "00110000" report "error on Apr" severity failure;
         w_sw <= x"5"; wait for 10 ns;
-            assert w_Y = '1' report "error on May" severity failure;
+            assert w_Y = "0010010" report "error on May" severity failure;
         w_sw <= x"6"; wait for 10 ns;
-            assert w_Y = '0' report "error on Jun" severity failure;
+            assert w_Y = "0000010" report "error on Jun" severity failure;
         w_sw <= x"7"; wait for 10 ns;
-            assert w_Y = '1' report "error on Jul" severity failure;
+            assert w_Y = "1111000" report "error on Jul" severity failure;
         w_sw <= x"8"; wait for 10 ns;
-            assert w_Y = '1' report "error on Aug" severity failure;
+            assert w_Y = "0000000" report "error on Aug" severity failure;
         w_sw <= x"9"; wait for 10 ns;
-            assert w_Y = '0' report "error on Sept" severity failure;         
+            assert w_Y = "0011000" report "error on Sept" severity failure;         
         w_sw <= x"A"; wait for 10 ns;
-            assert w_Y = '1' report "error on Oct" severity failure;
+            assert w_Y = "0001000" report "error on Oct" severity failure;
         w_sw <= x"B"; wait for 10 ns;
-            assert w_Y = '0' report "error on Nov" severity failure;
+            assert w_Y = "0000011" report "error on Nov" severity failure;
         w_sw <= x"C"; wait for 10 ns;
-            assert w_Y = '1' report "error on Dec" severity failure;      
-     --   w_sw <= x"13"; wait for 10 ns;
-    --        assert w_Y = '1' report "error on xD" severity failure;                    
-      --  w_sw <= x"14"; wait for 10 ns;
-     --       assert w_Y = '1' report "error on xE" severity failure;    
-    --    w_sw <= x"15"; wait for 10 ns;
-    --        assert w_Y = '1' report "error on xF" severity failure;                
+            assert w_Y = "0100111" report "error on Dec" severity failure;      
+       w_sw <= x"D"; wait for 10 ns;
+            assert w_Y = "0100001" report "error on xD" severity failure;                    
+        w_sw <= x"E"; wait for 10 ns;
+           assert w_Y = "0000110"  report "error on xE" severity failure;    
+        w_sw <= x"F"; wait for 10 ns;
+            assert w_Y = "0001110" report "error on xF" severity failure;                
      --    w_sw <= x"16"; wait for 10 ns;
       --      assert w_Y = '1' report "error on x10" severity failure;               
                 
